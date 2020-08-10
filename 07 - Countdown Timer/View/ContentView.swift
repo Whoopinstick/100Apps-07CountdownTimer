0a007114c6181ce916d7968445257d5e149f3ae4
Whoopinstick <k.m.paul68@gmail.com>
2020-08-10 16:41:37 -0400

    add countdown, but calculation is wrong.  minutes not syncing with seconds

--- 07 - Countdown Timer/View/ContentView.swift
+++ 07 - Countdown Timer/View/ContentView.swift
@@ -9,32 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedDate: Date = Date()
+    @State private var selectedDate: Date = Date().addingTimeInterval(86400)
+    @State private var currentDate = Date()
+    @State private var timer: Timer?
+    
+    var tomorrow: Date {
+        let currentDate = Date()
+        var dateComponent = DateComponents()
+        dateComponent.day = 1
+        let dayPlusOne = Calendar.current.date(byAdding: dateComponent, to: currentDate)
+        
+        return dayPlusOne ?? Date()
+        
+    }
+    
     var selectedDateStart: Date {
         let date = selectedDate
         let cal = Calendar(identifier: .gregorian)
-        let dateStart = cal.startOfDay(for: date)
-        return dateStart
+        let dateStart = cal.date(bySettingHour: 0, minute: 0, second: 0, of: date)
+        return dateStart ?? Date()
     }
+    
     var body: some View {
         VStack {
             Text("Select a date in the future:")
             
-            DatePicker("Select a date", selection: $selectedDate, in: Date()..., displayedComponents: .date)
+            DatePicker("Select a date", selection: $selectedDate, in: tomorrow..., displayedComponents: .date)
                 .labelsHidden()
             
             Button("Start Countdown") {
-                print("selected date: \(self.selectedDate)")
-                print("selected date start: \(self.selectedDateStart)")
+                self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    self.currentDate = Date()
+                }
             }
             
-            Text("current to selected start: \(Date().distance(to: selectedDateStart))")
+            Button("Print dates") {
+                print("now: \(Date())")
+                print("current: \(self.currentDate)")
+                print("selected: \(self.selectedDateStart)")
+                //print("tomorrow: \(self.tomorrow)")
+            }
+            
+            //Text("current to selected start: \(currentDate.distance(to: selectedDateStart))")
             
             Group {
-                Text("Days: \(Date().distance(to: selectedDateStart) / 24 / 60 / 60, specifier: "%.0f")")
-                Text("Hours: \((Date().distance(to: selectedDateStart)  / 60 / 60).truncatingRemainder(dividingBy: 24.0), specifier: "%.0f")")
-                Text("Minutes: \((Date().distance(to: selectedDateStart) / 60).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
-                Text("Seconds: \(Date().distance(to: selectedDateStart).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
+                Text("Days: \(currentDate.distance(to: selectedDateStart) / 86400, specifier: "%.0f")")
+                Text("Hours: \((currentDate.distance(to: selectedDateStart)  / 60 / 60).truncatingRemainder(dividingBy: 24.0), specifier: "%.0f")")
+                Text("Minutes: \((currentDate.distance(to: selectedDateStart) / 60).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
+                Text("Seconds: \(currentDate.distance(to: selectedDateStart).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
+                
+                
             }
             
         }
