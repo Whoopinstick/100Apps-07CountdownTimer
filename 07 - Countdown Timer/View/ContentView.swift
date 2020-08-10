80d9219172f006111aef9cb365aecbc612c3b045
Whoopinstick <k.m.paul68@gmail.com>
2020-08-10 15:24:34 -0400

    add basic components and calculate difference between current date and 12AM of selected date

--- 07 - Countdown Timer/View/ContentView.swift
+++ 07 - Countdown Timer/View/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedDate: Date = Date()
+    var selectedDateStart: Date {
+        let date = selectedDate
+        let cal = Calendar(identifier: .gregorian)
+        let dateStart = cal.startOfDay(for: date)
+        return dateStart
+    }
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Select a date in the future:")
+            
+            DatePicker("Select a date", selection: $selectedDate, in: Date()..., displayedComponents: .date)
+                .labelsHidden()
+            
+            Button("Start Countdown") {
+                print("selected date: \(self.selectedDate)")
+                print("selected date start: \(self.selectedDateStart)")
+            }
+            
+            Text("current to selected start: \(Date().distance(to: selectedDateStart))")
+            
+            Group {
+                Text("Days: \(Date().distance(to: selectedDateStart) / 24 / 60 / 60, specifier: "%.0f")")
+                Text("Hours: \((Date().distance(to: selectedDateStart)  / 60 / 60).truncatingRemainder(dividingBy: 24.0), specifier: "%.0f")")
+                Text("Minutes: \((Date().distance(to: selectedDateStart) / 60).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
+                Text("Seconds: \(Date().distance(to: selectedDateStart).truncatingRemainder(dividingBy: 60.0), specifier: "%.0f")")
+            }
+            
+        }
     }
 }
 
